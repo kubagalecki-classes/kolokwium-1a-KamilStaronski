63d00537d19846962fa934513f0b6faa1e7d10f5
KamilStaronski <73498649+KamilStaronski@users.noreply.github.com>
2021-01-14 13:07:43 +0100

    Update zad2.hpp

--- include/zad2.hpp
+++ include/zad2.hpp
@@ -5,5 +5,19 @@
 #include <type_traits>
 
 // tutaj klasa Penne
-
+class Penne : public Makaron {
+public:
+	double ileMaki(double wartosc) {
+		return wartosc;
+	}
+};
 // tutaj definicja metody gotujMakaron
+
+Makaron* Makaron::gotujMakaron(std::string& argument) {
+	if (argument.front() == argument.back()) {
+		return new Tagliatelle(3.14, 0.42, 0.1);
+	}
+	else {
+		return new Penne;
+	}
+}
