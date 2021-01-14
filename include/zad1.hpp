62848f7d1c14738557dece05e64c34bdbe8e9665
KamilStaronski <73498649+KamilStaronski@users.noreply.github.com>
2021-01-14 12:34:08 +0100

    Update zad1.hpp

--- include/zad1.hpp
+++ include/zad1.hpp
@@ -6,3 +6,17 @@
 
 // Zad1
 // tutaj definicja klasy Tagliatelle
+class Tagliatelle {
+private:
+	double L;
+	double W;
+	double R;
+	static const double C;
+public:
+	Tagliatelle(double l, double w, double r) :L(l), W(w), R(r){}
+  Tagliatelle() :L(0.5), W(0.5), R(0.5){}
+  double ileMaki(unsigned P) const{
+      double masa=P*L*W*(1.-R)*C;
+      return masa;
+  }
+};
